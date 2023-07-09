1dbc1918087fafd4ea93b16e93072532bef53691
Purnendu Kumar <purnenduk90@gmail.com>
2023-07-09 10:49:58 +0530

    out of index bug

--- score.py
+++ score.py
@@ -36,14 +36,7 @@ def get_score(fail_below):
     os.system("echo exit_code=" + exit_code + " >> $GITHUB_OUTPUT")
     return numeric_score
 
-def update_badge(readme_file_path, fail_below):
-
-    color_dict = create_color_dict(fail_below)
-    score = get_score(fail_below)
-    badge_color = get_badge_color(score, color_dict)
-
-    if not os.path.isfile(readme_file_path):
-        raise FileNotFoundError(f"README.md path is wrong, no file can be located at {readme_file_path}")
+def update_badge(readme_file_path, score, badge_color):
 
     with open(readme_file_path, "r", encoding="utf8") as f:
         content = f.read()
@@ -53,20 +46,30 @@ def update_badge(readme_file_path, fail_below):
     badge_pattern = r"(?<=!\[pylint]\()(.*?)(?=\))"
 
     if re.search(badge_pattern, content) is None:
-        content_split = content.split("\n")
-        # add badge in 2nd line
-        content_split[1] = "![pylint]() " + content_split[1] 
-        content = "\n".join(content_split)
+        if content.strip()[0] == "#":
+            content = "![pylint]() \n\n" + content
+        else:
+            content = "![pylint]() " + content
 
     result = re.sub(badge_pattern, badge_url, content)
     with open(readme_file_path, "w", encoding="utf8") as f:
         f.write(result)
 
+def main(readme_file_path, fail_below):
+
+    color_dict = create_color_dict(fail_below)
+    score = get_score(fail_below)
+    badge_color = get_badge_color(score, color_dict)
+
+    if readme_file_path:
+        if not os.path.isfile(readme_file_path):
+            raise FileNotFoundError(f"README.md path is wrong, no file can be located at {readme_file_path}")
+        
+        update_badge(readme_file_path, score, badge_color)
+
+        
 if __name__ == "__main__":
-    input_args = sys.argv[1:]
-    if len(input_args) < 1:
-        raise AttributeError("readme file path is required.")
-    file_path = input_args[0]
-    if len(input_args)>1:
-        fail_below = input_args[2]
-    update_badge(file_path, fail_below)
+    fail_below = sys.argv[1]
+    file_path = sys.argv[2]
+
+    main(file_path, fail_below)
